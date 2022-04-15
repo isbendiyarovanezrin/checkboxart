89a4efc0a1371b5bbe348a9b37dc82d07142197d
Nəzrin <isbndyrva@gmail.com>
2022-04-15 10:18:15 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -54,178 +54,189 @@ if (desktop.matches) {
   }
 }
 
-document.querySelector("[data-div='1'] [data-input='14']").checked = true;
-document.querySelector("[data-div='1'] [data-input='32']").checked = true;
-document.querySelector("[data-div='2'] [data-input='13']").checked = true;
-document.querySelector("[data-div='2'] [data-input='33']").checked = true;
-document.querySelector("[data-div='3'] [data-input='12']").checked = true;
-document.querySelector("[data-div='3'] [data-input='34']").checked = true;
-document.querySelector("[data-div='4'] [data-input='11']").checked = true;
-document.querySelector("[data-div='4'] [data-input='35']").checked = true;
-document.querySelector("[data-div='5'] [data-input='10']").checked = true;
-document.querySelector("[data-div='5'] [data-input='36']").checked = true;
-document.querySelector("[data-div='6'] [data-input='10']").checked = true;
-document.querySelector("[data-div='6'] [data-input='36']").checked = true;
-document.querySelector("[data-div='7'] [data-input='10']").checked = true;
-document.querySelector("[data-div='7'] [data-input='36']").checked = true;
-document.querySelector("[data-div='8'] [data-input='10']").checked = true;
-document.querySelector("[data-div='8'] [data-input='15']").checked = true;
-document.querySelector("[data-div='8'] [data-input='16']").checked = true;
-document.querySelector("[data-div='8'] [data-input='17']").checked = true;
-document.querySelector("[data-div='8'] [data-input='18']").checked = true;
-document.querySelector("[data-div='8'] [data-input='28']").checked = true;
-document.querySelector("[data-div='8'] [data-input='29']").checked = true;
-document.querySelector("[data-div='8'] [data-input='30']").checked = true;
-document.querySelector("[data-div='8'] [data-input='31']").checked = true;
-document.querySelector("[data-div='8'] [data-input='36']").checked = true;
-document.querySelector("[data-div='9'] [data-input='10']").checked = true;
-document.querySelector("[data-div='9'] [data-input='14']").checked = true;
-document.querySelector("[data-div='9'] [data-input='16']").checked = true;
-document.querySelector("[data-div='9'] [data-input='17']").checked = true;
-document.querySelector("[data-div='9'] [data-input='19']").checked = true;
-document.querySelector("[data-div='9'] [data-input='27']").checked = true;
-document.querySelector("[data-div='9'] [data-input='29']").checked = true;
-document.querySelector("[data-div='9'] [data-input='30']").checked = true;
-document.querySelector("[data-div='9'] [data-input='32']").checked = true;
-document.querySelector("[data-div='9'] [data-input='36']").checked = true;
-document.querySelector("[data-div='9'] [data-input='37']").checked = true;
-document.querySelector("[data-div='9'] [data-input='38']").checked = true;
-document.querySelector("[data-div='9'] [data-input='39']").checked = true;
-document.querySelector("[data-div='10'] [data-input='10']").checked = true;
-document.querySelector("[data-div='10'] [data-input='14']").checked = true;
-document.querySelector("[data-div='10'] [data-input='16']").checked = true;
-document.querySelector("[data-div='10'] [data-input='17']").checked = true;
-document.querySelector("[data-div='10'] [data-input='19']").checked = true;
-document.querySelector("[data-div='10'] [data-input='27']").checked = true;
-document.querySelector("[data-div='10'] [data-input='29']").checked = true;
-document.querySelector("[data-div='10'] [data-input='30']").checked = true;
-document.querySelector("[data-div='10'] [data-input='32']").checked = true;
-document.querySelector("[data-div='10'] [data-input='36']").checked = true;
-document.querySelector("[data-div='10'] [data-input='39']").checked = true;
-document.querySelector("[data-div='11'] [data-input='7']").checked = true;
-document.querySelector("[data-div='11'] [data-input='8']").checked = true;
-document.querySelector("[data-div='11'] [data-input='9']").checked = true;
-document.querySelector("[data-div='11'] [data-input='10']").checked = true;
-document.querySelector("[data-div='11'] [data-input='15']").checked = true;
-document.querySelector("[data-div='11'] [data-input='16']").checked = true;
-document.querySelector("[data-div='11'] [data-input='17']").checked = true;
-document.querySelector("[data-div='11'] [data-input='18']").checked = true;
-document.querySelector("[data-div='11'] [data-input='28']").checked = true;
-document.querySelector("[data-div='11'] [data-input='29']").checked = true;
-document.querySelector("[data-div='11'] [data-input='30']").checked = true;
-document.querySelector("[data-div='11'] [data-input='31']").checked = true;
-document.querySelector("[data-div='11'] [data-input='36']").checked = true;
-document.querySelector("[data-div='11'] [data-input='39']").checked = true;
-document.querySelector("[data-div='12'] [data-input='7']").checked = true;
-document.querySelector("[data-div='12'] [data-input='10']").checked = true;
-document.querySelector("[data-div='12'] [data-input='36']").checked = true;
-document.querySelector("[data-div='12'] [data-input='38']").checked = true;
-document.querySelector("[data-div='13'] [data-input='7']").checked = true;
-document.querySelector("[data-div='13'] [data-input='10']").checked = true;
-document.querySelector("[data-div='13'] [data-input='36']").checked = true;
-document.querySelector("[data-div='13'] [data-input='37']").checked = true;
-document.querySelector("[data-div='14'] [data-input='8']").checked = true;
-document.querySelector("[data-div='14'] [data-input='10']").checked = true;
-document.querySelector("[data-div='14'] [data-input='36']").checked = true;
-document.querySelector("[data-div='15'] [data-input='9']").checked = true;
-document.querySelector("[data-div='15'] [data-input='10']").checked = true;
-document.querySelector("[data-div='15'] [data-input='36']").checked = true;
-document.querySelector("[data-div='16'] [data-input='10']").checked = true;
-document.querySelector("[data-div='16'] [data-input='21']").checked = true;
-document.querySelector("[data-div='16'] [data-input='25']").checked = true;
-document.querySelector("[data-div='16'] [data-input='36']").checked = true;
-document.querySelector("[data-div='17'] [data-input='10']").checked = true;
-document.querySelector("[data-div='17'] [data-input='22']").checked = true;
-document.querySelector("[data-div='17'] [data-input='23']").checked = true;
-document.querySelector("[data-div='17'] [data-input='24']").checked = true;
-document.querySelector("[data-div='17'] [data-input='36']").checked = true;
-document.querySelector("[data-div='18'] [data-input='10']").checked = true;
-document.querySelector("[data-div='18'] [data-input='36']").checked = true;
-document.querySelector("[data-div='19'] [data-input='10']").checked = true;
-document.querySelector("[data-div='19'] [data-input='36']").checked = true;
-document.querySelector("[data-div='20'] [data-input='10']").checked = true;
-document.querySelector("[data-div='20'] [data-input='36']").checked = true;
-document.querySelector("[data-div='21'] [data-input='11']").checked = true;
-document.querySelector("[data-div='21'] [data-input='36']").checked = true;
-document.querySelector("[data-div='22'] [data-input='12']").checked = true;
-document.querySelector("[data-div='22'] [data-input='19']").checked = true;
-document.querySelector("[data-div='22'] [data-input='20']").checked = true;
-document.querySelector("[data-div='22'] [data-input='21']").checked = true;
-document.querySelector("[data-div='22'] [data-input='22']").checked = true;
-document.querySelector("[data-div='22'] [data-input='23']").checked = true;
-document.querySelector("[data-div='22'] [data-input='24']").checked = true;
-document.querySelector("[data-div='22'] [data-input='25']").checked = true;
-document.querySelector("[data-div='22'] [data-input='26']").checked = true;
-document.querySelector("[data-div='22'] [data-input='27']").checked = true;
-document.querySelector("[data-div='22'] [data-input='28']").checked = true;
-document.querySelector("[data-div='22'] [data-input='35']").checked = true;
-document.querySelector("[data-div='23'] [data-input='13']").checked = true;
-document.querySelector("[data-div='23'] [data-input='19']").checked = true;
-document.querySelector("[data-div='23'] [data-input='20']").checked = true;
-document.querySelector("[data-div='23'] [data-input='21']").checked = true;
-document.querySelector("[data-div='23'] [data-input='22']").checked = true;
-document.querySelector("[data-div='23'] [data-input='23']").checked = true;
-document.querySelector("[data-div='23'] [data-input='24']").checked = true;
-document.querySelector("[data-div='23'] [data-input='25']").checked = true;
-document.querySelector("[data-div='23'] [data-input='26']").checked = true;
-document.querySelector("[data-div='23'] [data-input='27']").checked = true;
-document.querySelector("[data-div='23'] [data-input='28']").checked = true;
-document.querySelector("[data-div='23'] [data-input='34']").checked = true;
-document.querySelector("[data-div='24'] [data-input='14']").checked = true;
-document.querySelector("[data-div='24'] [data-input='20']").checked = true;
-document.querySelector("[data-div='24'] [data-input='21']").checked = true;
-document.querySelector("[data-div='24'] [data-input='22']").checked = true;
-document.querySelector("[data-div='24'] [data-input='23']").checked = true;
-document.querySelector("[data-div='24'] [data-input='24']").checked = true;
-document.querySelector("[data-div='24'] [data-input='25']").checked = true;
-document.querySelector("[data-div='24'] [data-input='26']").checked = true;
-document.querySelector("[data-div='24'] [data-input='27']").checked = true;
-document.querySelector("[data-div='24'] [data-input='33']").checked = true;
-document.querySelector("[data-div='25'] [data-input='15']").checked = true;
-document.querySelector("[data-div='25'] [data-input='21']").checked = true;
-document.querySelector("[data-div='25'] [data-input='22']").checked = true;
-document.querySelector("[data-div='25'] [data-input='23']").checked = true;
-document.querySelector("[data-div='25'] [data-input='24']").checked = true;
-document.querySelector("[data-div='25'] [data-input='25']").checked = true;
-document.querySelector("[data-div='25'] [data-input='26']").checked = true;
-document.querySelector("[data-div='25'] [data-input='24']").checked = true;
-document.querySelector("[data-div='25'] [data-input='32']").checked = true;
-document.querySelector("[data-div='26'] [data-input='16']").checked = true;
-document.querySelector("[data-div='26'] [data-input='31']").checked = true;
-document.querySelector("[data-div='27'] [data-input='17']").checked = true;
-document.querySelector("[data-div='27'] [data-input='30']").checked = true;
-document.querySelector("[data-div='28'] [data-input='18']").checked = true;
-document.querySelector("[data-div='28'] [data-input='29']").checked = true;
-document.querySelector("[data-div='29'] [data-input='19']").checked = true;
-document.querySelector("[data-div='29'] [data-input='20']").checked = true;
-document.querySelector("[data-div='29'] [data-input='21']").checked = true;
-document.querySelector("[data-div='29'] [data-input='22']").checked = true;
-document.querySelector("[data-div='29'] [data-input='23']").checked = true;
-document.querySelector("[data-div='29'] [data-input='24']").checked = true;
-document.querySelector("[data-div='29'] [data-input='25']").checked = true;
-document.querySelector("[data-div='29'] [data-input='26']").checked = true;
-document.querySelector("[data-div='29'] [data-input='27']").checked = true;
-document.querySelector("[data-div='29'] [data-input='28']").checked = true;
-document.querySelector("[data-div='30'] [data-input='23']").checked = true;
-document.querySelector("[data-div='31'] [data-input='23']").checked = true;
-document.querySelector("[data-div='32'] [data-input='23']").checked = true;
-document.querySelector("[data-div='33'] [data-input='22']").checked = true;
-document.querySelector("[data-div='33'] [data-input='23']").checked = true;
-document.querySelector("[data-div='33'] [data-input='24']").checked = true;
-document.querySelector("[data-div='34'] [data-input='20']").checked = true;
-document.querySelector("[data-div='34'] [data-input='21']").checked = true;
-document.querySelector("[data-div='34'] [data-input='23']").checked = true;
-document.querySelector("[data-div='34'] [data-input='25']").checked = true;
-document.querySelector("[data-div='34'] [data-input='26']").checked = true;
-document.querySelector("[data-div='35'] [data-input='23']").checked = true;
-document.querySelector("[data-div='36'] [data-input='23']").checked = true;
-document.querySelector("[data-div='37'] [data-input='23']").checked = true;
-document.querySelector("[data-div='38'] [data-input='23']").checked = true;
-document.querySelector("[data-div='39'] [data-input='22']").checked = true;
-document.querySelector("[data-div='39'] [data-input='24']").checked = true;
-document.querySelector("[data-div='40'] [data-input='21']").checked = true;
-document.querySelector("[data-div='40'] [data-input='25']").checked = true;
+// checked checkbox positions
+const position = [
+  { div: 1, input: 14 },
+  { div: 1, input: 32 },
+  { div: 2, input: 13 },
+  { div: 2, input: 33 },
+  { div: 3, input: 12 },
+  { div: 3, input: 34 },
+  { div: 4, input: 11 },
+  { div: 4, input: 35 },
+  { div: 5, input: 10 },
+  { div: 5, input: 36 },
+  { div: 6, input: 10 },
+  { div: 6, input: 36 },
+  { div: 7, input: 10 },
+  { div: 7, input: 36 },
+  { div: 8, input: 10 },
+  { div: 8, input: 15 },
+  { div: 8, input: 16 },
+  { div: 8, input: 17 },
+  { div: 8, input: 18 },
+  { div: 8, input: 18 },
+  { div: 8, input: 18 },
+  { div: 8, input: 28 },
+  { div: 8, input: 29 },
+  { div: 8, input: 30 },
+  { div: 8, input: 31 },
+  { div: 8, input: 36 },
+  { div: 9, input: 10 },
+  { div: 9, input: 14 },
+  { div: 9, input: 16 },
+  { div: 9, input: 17 },
+  { div: 9, input: 19 },
+  { div: 9, input: 27 },
+  { div: 9, input: 29 },
+  { div: 9, input: 30 },
+  { div: 9, input: 32 },
+  { div: 9, input: 36 },
+  { div: 9, input: 37 },
+  { div: 9, input: 38 },
+  { div: 9, input: 39 },
+  { div: 10, input: 10 },
+  { div: 10, input: 14 },
+  { div: 10, input: 16 },
+  { div: 10, input: 17 },
+  { div: 10, input: 19 },
+  { div: 10, input: 27 },
+  { div: 10, input: 29 },
+  { div: 10, input: 30 },
+  { div: 10, input: 32 },
+  { div: 10, input: 36 },
+  { div: 10, input: 39 },
+  { div: 11, input: 7 },
+  { div: 11, input: 8 },
+  { div: 11, input: 9 },
+  { div: 11, input: 10 },
+  { div: 11, input: 15 },
+  { div: 11, input: 16 },
+  { div: 11, input: 17 },
+  { div: 11, input: 18 },
+  { div: 11, input: 28 },
+  { div: 11, input: 29 },
+  { div: 11, input: 30 },
+  { div: 11, input: 31 },
+  { div: 11, input: 36 },
+  { div: 11, input: 39 },
+  { div: 12, input: 7 },
+  { div: 12, input: 10 },
+  { div: 12, input: 36 },
+  { div: 12, input: 38 },
+  { div: 13, input: 7 },
+  { div: 13, input: 10 },
+  { div: 13, input: 36 },
+  { div: 13, input: 37 },
+  { div: 14, input: 8 },
+  { div: 14, input: 10 },
+  { div: 14, input: 36 },
+  { div: 15, input: 9 },
+  { div: 15, input: 10 },
+  { div: 15, input: 36 },
+  { div: 16, input: 10 },
+  { div: 16, input: 21 },
+  { div: 16, input: 25 },
+  { div: 16, input: 36 },
+  { div: 17, input: 10 },
+  { div: 17, input: 22 },
+  { div: 17, input: 23 },
+  { div: 17, input: 24 },
+  { div: 17, input: 36 },
+  { div: 18, input: 10 },
+  { div: 18, input: 36 },
+  { div: 19, input: 10 },
+  { div: 19, input: 36 },
+  { div: 20, input: 10 },
+  { div: 20, input: 36 },
+  { div: 21, input: 11 },
+  { div: 21, input: 36 },
+  { div: 22, input: 12 },
+  { div: 22, input: 19 },
+  { div: 22, input: 20 },
+  { div: 22, input: 21 },
+  { div: 22, input: 22 },
+  { div: 22, input: 23 },
+  { div: 22, input: 24 },
+  { div: 22, input: 25 },
+  { div: 22, input: 26 },
+  { div: 22, input: 27 },
+  { div: 22, input: 28 },
+  { div: 22, input: 35 },
+  { div: 23, input: 13 },
+  { div: 23, input: 19 },
+  { div: 23, input: 20 },
+  { div: 23, input: 21 },
+  { div: 23, input: 22 },
+  { div: 23, input: 23 },
+  { div: 23, input: 24 },
+  { div: 23, input: 25 },
+  { div: 23, input: 26 },
+  { div: 23, input: 27 },
+  { div: 23, input: 28 },
+  { div: 23, input: 34 },
+  { div: 24, input: 14 },
+  { div: 24, input: 20 },
+  { div: 24, input: 21 },
+  { div: 24, input: 22 },
+  { div: 24, input: 23 },
+  { div: 24, input: 24 },
+  { div: 24, input: 25 },
+  { div: 24, input: 26 },
+  { div: 24, input: 27 },
+  { div: 24, input: 33 },
+  { div: 25, input: 15 },
+  { div: 25, input: 21 },
+  { div: 25, input: 22 },
+  { div: 25, input: 23 },
+  { div: 25, input: 24 },
+  { div: 25, input: 25 },
+  { div: 25, input: 26 },
+  { div: 25, input: 32 },
+  { div: 26, input: 16 },
+  { div: 26, input: 31 },
+  { div: 27, input: 17 },
+  { div: 27, input: 30 },
+  { div: 28, input: 18 },
+  { div: 28, input: 29 },
+  { div: 29, input: 19 },
+  { div: 29, input: 20 },
+  { div: 29, input: 21 },
+  { div: 29, input: 22 },
+  { div: 29, input: 23 },
+  { div: 29, input: 24 },
+  { div: 29, input: 25 },
+  { div: 29, input: 26 },
+  { div: 29, input: 27 },
+  { div: 29, input: 28 },
+  { div: 30, input: 23 },
+  { div: 31, input: 23 },
+  { div: 32, input: 23 },
+  { div: 33, input: 22 },
+  { div: 33, input: 23 },
+  { div: 33, input: 24 },
+  { div: 34, input: 20 },
+  { div: 34, input: 21 },
+  { div: 34, input: 23 },
+  { div: 34, input: 25 },
+  { div: 34, input: 26 },
+  { div: 35, input: 23 },
+  { div: 36, input: 23 },
+  { div: 37, input: 23 },
+  { div: 38, input: 23 },
+  { div: 39, input: 22 },
+  { div: 39, input: 24 },
+  { div: 40, input: 21 },
+  { div: 40, input: 25 },
+];
 
+for (let i = 0; i < position.length; i++) {
+  document.querySelector(
+    `[data-div='${position[i].div}'] [data-input='${position[i].input}']`
+  ).checked = true;
+}
+
+// sound effect
 const inputs = document.querySelectorAll("input");
 const sound = document.getElementById("sound");
 
